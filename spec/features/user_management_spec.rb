02924252f14e668e3a8db331fa6dcb381f0fe6d9
Costas Kiteou <ckiteou22@gmail.com>
2017-07-27 17:29:14 +0100

    Add password confirmation functionality

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -6,4 +6,11 @@ feature 'User sign up' do
     expect(page).to have_content('Welcome, alice@example.com')
     expect(User.first.email).to eq('alice@example.com')
   end
+
+  scenario 'requires a matching confirmation password' do
+    # again it's questionable whether we should be testing the model at this
+    # level.  We are mixing integration tests with feature tests.
+    # However, it's convenient for our purposes.
+    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+  end
 end
