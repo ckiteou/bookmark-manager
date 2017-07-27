8931b107d74c1a71363c3c325657cd0560939c10
Costas Kiteou <ckiteou22@gmail.com>
2017-07-27 22:12:13 +0100

    Add 'sinatra-flash' to display a message that'Password and confirmation password do not match'. Add functionality so that the user's email is persisted in the signup form after a password confirmation mismatch

--- app/app.rb
+++ app/app.rb
@@ -1,21 +1,29 @@
 ENV["RACK_ENV"] ||= "development"
 require 'sinatra/base'
+require 'sinatra/flash'
 require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
+  register Sinatra::Flash
   enable :sessions
   set :session_secret, 'super secret'
 
   get '/users/new' do
+    @user = User.new
     erb :'users/new'
   end
 
   post '/users' do
-    user = User.create(email: params[:email],
+    @user = User.create(email: params[:email],
                        password: params[:password],
                        password_confirmation: params[:password_confirmation])
-    session[:user_id] = user.id
-    redirect to('/links')
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to('/links')
+    else
+      flash.now[:notice] = "Password and confirmation password do not match"
+      erb :'users/new'
+    end
   end
 
   get '/links' do
@@ -48,7 +56,7 @@ class BookmarkManager < Sinatra::Base
    def current_user
      @current_user ||= User.get(session[:user_id])
    end
-  end
+ end
 
   run! if app_file == $0
 end
